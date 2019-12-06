5075eaa5084445014a72fda51d6b75755a926d48
Jeremy Shaw <jjsjeremy61@>
2019-12-05 19:14:05 -0800

    Added the ability to add a question

---
+++ src/main/java/edu/csumb/Webstore/model/Question.java
@@ -0,0 +1,74 @@
+//This is your model. This file should only contain getters and setters, along with the variables your struct uses.
+//DONT FORGET THE CONSTRUCTOR! Getters and setters must be setup to match the name exactly
+//or else you will have many errors.
+package edu.csumb.Webstore.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Question
+{
+    String question;
+    @Id
+    String id;
+    String description;
+    Integer value;
+    String a;
+    String b;
+    String c;
+    String answer;
+
+    public Question() {
+        question = "Default";
+        description = "This is the description of a default question";
+        a = "this is a";
+        b = "this is b";
+        c = "this is c";
+        answer = "a";
+        value = 1;
+    }
+    public String getQuestion() {
+        return question;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getA() {
+        return a;
+    }
+    public String getB() {
+        return b;
+    }
+    public String getC() {
+        return c;
+    }
+    public String answer() {
+        return answer;
+    }
+    public Integer getValue() {
+        return value;
+    }
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setA(String a) {
+        this.a = a;
+    }
+    public void setB(String b) {
+        this.b = b;
+    }
+    public void setC(String c) {
+        this.c = c;
+    }
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/edu/csumb/Webstore/service/QuestionService.java
@@ -0,0 +1,35 @@
+//The service file is where the heavy lifting is done.
+//You will do all logic, all database access(Special database operations defined in the repository).
+//Basically all your actual code is here!
+package edu.csumb.Webstore.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.csumb.Webstore.model.Question;
+import edu.csumb.Webstore.repositories.QuestionRepository;
+
+//Remember to annotate what type of class this is!
+@Service
+public class QuestionService
+{
+    @Autowired
+    private QuestionRepository repo;
+
+    public Iterable<Question> getAll()
+    {
+        return repo.findAll();
+    }
+
+    public Question getById(String id) {
+        return repo.findById(id).get();
+    }
+
+    public Question addQuestion(Question myQuestion) {
+        return repo.save(myQuestion);
+    }
+}
